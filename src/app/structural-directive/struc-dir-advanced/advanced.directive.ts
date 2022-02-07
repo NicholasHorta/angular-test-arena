0381cc6db80220ada5c9c75fc5f6b04e4a6ce7d0
NicholasHorta <nickdeveloper.js@gmil.com>
2022-02-07 21:47:03 +0000

    structural directives update

--- src/app/structural-directive/struc-dir-advanced/advanced.directive.ts
+++ src/app/structural-directive/struc-dir-advanced/advanced.directive.ts
@@ -1,12 +1,16 @@
 import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 class HideAfterContext {
+  public get $implicit(){
+    return this.advancedDir;
+  }
   public advancedDir = 0;
+    
+  public counter = 0;
 }
 
 @Directive({
   selector: '[advancedDir]'
 })
-
 
 export class AdvancedDirective implements OnInit {
 
@@ -15,25 +19,35 @@ export class AdvancedDirective implements OnInit {
   @Input() 
   set advancedDir(value: number | null){
     this._advancedDir = value ?? 0;
-    this.context.advancedDir = this._advancedDir / 1000;
+    this.context.advancedDir = this.context.counter = this._advancedDir / 1000;
   }
-  private _advancedDir = 0;
-
-
   @Input('advancedDirPost') placeholder: TemplateRef<any> | null = null;
-  
+
   private context = new HideAfterContext();
+
+  private _advancedDir = 0;
 
   ngOnInit(): void {
     this.viewContRef.createEmbeddedView(this.templateRef, this.context);
 
+    const intervalId = setInterval(() => {
+      this.context.counter--
+      console.log(this.context.counter);
+    }, 1000);
+
     setTimeout(() => {
       this.viewContRef.clear();
       if(this.placeholder){
-        this.viewContRef.createEmbeddedView(this.placeholder)
+        this.viewContRef.createEmbeddedView(this.placeholder, {
+            $implicit: 'DEFAULT',
+            crisis: alert('CRISYS'),
+            message: "Here is the message we have attached to the context object"
+        })
       }
+      clearInterval(intervalId);
     }, this._advancedDir)
   }
-
 }
+
+
 
